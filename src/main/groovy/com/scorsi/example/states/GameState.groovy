0d370330450dee6056a2dabfe5bf5fcdbe23aa80
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 19:20:28 +0100

    Add World uses example

--- src/main/groovy/com/scorsi/example/states/GameState.groovy
+++ src/main/groovy/com/scorsi/example/states/GameState.groovy
@@ -3,8 +3,10 @@ package com.scorsi.example.states
 import com.scorsi.example.entities.Player
 import com.scorsi.example.graphics.tiles.DirtTile
 import com.scorsi.example.graphics.tiles.GrassTile
+import com.scorsi.example.graphics.tiles.StoneTile
 import com.scorsi.gameengine.Game
 import com.scorsi.gameengine.graphics.Tile
+import com.scorsi.gameengine.graphics.World
 import com.scorsi.gameengine.states.State
 import com.scorsi.gameengine.utils.Position2D
 
@@ -12,9 +14,12 @@ import java.awt.Graphics
 
 class GameState extends State {
 
+    World world
     Player player
+
     Tile dirtTile
     Tile grassTile
+    Tile stoneTile
 
     GameState(Game game) {
         super(game)
@@ -25,16 +30,19 @@ class GameState extends State {
         player = new Player(game, new Position2D(100, 100))
         grassTile = new GrassTile(0)
         dirtTile = new DirtTile(1)
+        stoneTile = new StoneTile(2)
+        world = new World("/worlds/world1.txt")
     }
 
     @Override
     void update() {
+        world.update()
         player.update()
     }
 
     @Override
     void render(Graphics g) {
-        Tile.tiles[0].render(g, 0, 0)
+        world.render(g)
         player.render(g)
     }
 
