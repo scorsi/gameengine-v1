b7cc3037241e63078aae5fad6c4ea3fa34a7c724
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 01:30:25 +0100

    Add the overriding Game class

---
+++ src/main/groovy/com/scorsi/gameengine/MyGame.groovy
@@ -0,0 +1,15 @@
+package com.scorsi.gameengine
+
+class MyGame extends Game {
+    @Override
+    protected void init() throws Exception {
+    }
+
+    @Override
+    protected void update() throws Exception {
+    }
+
+    @Override
+    protected void render() throws Exception {
+    }
+}
