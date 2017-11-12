c2bb27bf33c3e941a8c9214c3984fa171469282d
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 16:13:40 +0100

    Add height and width parameter to Entities

--- src/main/groovy/com/scorsi/gameengine/entities/Character.groovy
+++ src/main/groovy/com/scorsi/gameengine/entities/Character.groovy
@@ -5,11 +5,18 @@ import com.scorsi.gameengine.utils.Position2D
 
 abstract class Character extends Entity {
 
-    Double health
+    static final DEFAULT_HEALTH = 100D
+    static final DEFAULT_CREATURE_WIDTH = 64
+    static final DEFAULT_CREATURE_HEIGHT = 64
+
+    Double health = DEFAULT_HEALTH
 
     Character(Game game, Position2D pos) {
-        super(game, pos)
-        health = 0
+        super(game, pos, DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT)
+    }
+
+    Character(Game game, Position2D pos, Integer width, Integer height) {
+        super(game, pos, width, height)
     }
 
 }
--- src/main/groovy/com/scorsi/gameengine/entities/Entity.groovy
+++ src/main/groovy/com/scorsi/gameengine/entities/Entity.groovy
@@ -8,11 +8,15 @@ import java.awt.Graphics
 abstract class Entity {
 
     protected Game game
-    protected Position2D position
+    Position2D position
+    Integer width
+    Integer height
 
-    Entity(Game game, Position2D position) {
+    Entity(Game game, Position2D position, Integer width, Integer height) {
         this.game = game
         this.position = position
+        this.width = width
+        this.height = height
     }
 
     abstract void update()
