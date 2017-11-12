f8af0c37c6fe976ef1ffdf640c43591bcc997c43
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 19:18:59 +0100

    Add new Tile example

---
+++ src/main/groovy/com/scorsi/example/graphics/tiles/StoneTile.groovy
@@ -0,0 +1,12 @@
+package com.scorsi.example.graphics.tiles
+
+import com.scorsi.example.Assets
+import com.scorsi.gameengine.graphics.Tile
+
+class StoneTile extends Tile {
+
+    StoneTile(Integer id) {
+        super(Assets.stone, id)
+    }
+
+}
