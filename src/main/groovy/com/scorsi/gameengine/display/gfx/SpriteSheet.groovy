c4ab5fccbaa76daffebbbeb1969a413172647749
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 13:15:16 +0100

    Add SpriteSheet

---
+++ src/main/groovy/com/scorsi/gameengine/display/gfx/SpriteSheet.groovy
@@ -0,0 +1,21 @@
+package com.scorsi.gameengine.display.gfx
+
+import java.awt.image.BufferedImage
+
+class SpriteSheet {
+
+    BufferedImage sheet
+
+    SpriteSheet(BufferedImage image) {
+        sheet = image
+    }
+
+    SpriteSheet(String path) {
+        sheet = ImageLoader.load(path)
+    }
+
+    BufferedImage crop(Integer x, Integer y, Integer width, Integer height) {
+        return sheet.getSubimage(x, y, width, height)
+    }
+
+}
