8789042de9496753af41d1aaf833462c7f9f241c
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 15:49:37 +0100

    Add Position utils

---
+++ src/main/groovy/com/scorsi/gameengine/utils/Position2D.groovy
@@ -0,0 +1,18 @@
+package com.scorsi.gameengine.utils
+
+class Position2D {
+
+    Double x
+    Double y
+
+    Position2D() {
+        x = 0
+        y = 0
+    }
+
+    Position2D(Double x, Double y) {
+        this.x = x
+        this.y = y
+    }
+
+}
