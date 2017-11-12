c217c7f3af7b9689f1e600ac235709de3b421cc3
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 13:12:13 +0100

    Rename TextureLoader to ImageLoader

--- src/main/groovy/com/scorsi/gameengine/display/gfx/ImageLoader.groovy
+++ src/main/groovy/com/scorsi/gameengine/display/gfx/ImageLoader.groovy
@@ -3,11 +3,11 @@ package com.scorsi.gameengine.display.gfx
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 
-class TextureLoader {
+class ImageLoader {
 
-    static BufferedImage loadTexture(String path) {
+    static BufferedImage load(String path) {
         try {
-            return ImageIO.read(TextureLoader.class.getResource(path))
+            return ImageIO.read(ImageLoader.class.getResource(path))
         } catch (IOException e) {
             e.printStackTrace()
             System.exit(1)
