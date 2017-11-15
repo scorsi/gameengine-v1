7f65aff905f810af822acfc49041f69b66ac0364
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-15 10:32:54 +0100

    Change default assets.

--- src/main/groovy/com/scorsi/example/Assets.groovy
+++ src/main/groovy/com/scorsi/example/Assets.groovy
@@ -17,11 +17,11 @@ class Assets {
     static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.load("/textures/sheet.png"))
 
-        player = sheet.crop(0, 0, width, height)
+        tree = sheet.crop(0, 0, width, height)
         dirt = sheet.crop(width, 0, width, height)
         grass = sheet.crop(width * 2, 0, width, height)
         stone = sheet.crop(width * 3, 0, width, height)
-        tree = sheet.crop(0, height, width, height)
+        player = sheet.crop(width * 4, 0, width, height)
     }
 
 }
