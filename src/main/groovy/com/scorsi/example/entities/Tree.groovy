7d41871481c8b459c1fc155a5cae7cc247f709b0
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-16 01:30:45 +0100

    Tree is Damageable

--- src/main/groovy/com/scorsi/example/entities/Tree.groovy
+++ src/main/groovy/com/scorsi/example/entities/Tree.groovy
@@ -3,13 +3,14 @@ package com.scorsi.example.entities
 import com.scorsi.example.Assets
 import com.scorsi.gameengine.Handler
 import com.scorsi.gameengine.entities.Collidable
+import com.scorsi.gameengine.entities.Damageable
 import com.scorsi.gameengine.entities.Entity
 import com.scorsi.gameengine.entities.Texturable
 import com.scorsi.gameengine.graphics.Tile
 import com.scorsi.gameengine.utils.Position2D
 import com.scorsi.gameengine.utils.Rectangle2D
 
-class Tree extends Entity implements Texturable, Collidable {
+class Tree extends Entity implements Damageable, Texturable, Collidable {
 
     Tree(Handler handler, Position2D position) {
         super(handler)
@@ -26,4 +27,9 @@ class Tree extends Entity implements Texturable, Collidable {
         this.collisionBox = new Rectangle2D(10, height / 1.5D, width - 20, height - height / 1.5D)
     }
 
+    @Override
+    void takeDamage(Double damage) {
+        Damageable.super.takeDamage(damage)
+        println(id + " has been damaged")
+    }
 }
