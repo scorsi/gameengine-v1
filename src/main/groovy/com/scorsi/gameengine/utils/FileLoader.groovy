dfdc526a7a0394c7e09567dc97b172bd6d4b5bed
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-21 13:07:26 +0100

    Add ImageLoader and read from actual jar directory

--- src/main/groovy/com/scorsi/gameengine/utils/FileLoader.groovy
+++ src/main/groovy/com/scorsi/gameengine/utils/FileLoader.groovy
@@ -1,12 +1,18 @@
 package com.scorsi.gameengine.utils
 
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
 class FileLoader {
+
+    static final String RESOURCES_DIRECTORY = "./resources/"
 
     static String loadFileAsString(String path) {
         StringBuilder builder = new StringBuilder()
 
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(FileLoader.class.getResourceAsStream(path)))
+            //BufferedReader br = new BufferedReader(new InputStreamReader(FileLoader.class.getResourceAsStream(path)))
+            BufferedReader br = new BufferedReader(new FileReader(RESOURCES_DIRECTORY + path))
             String line
             while ((line = br.readLine()) != null) {
                 builder.append(line + "\n")
@@ -14,9 +20,19 @@ class FileLoader {
             br.close()
         } catch (IOException e) {
             e.printStackTrace()
+            throw new Exception("File " + RESOURCES_DIRECTORY + path + " not found")
         }
 
         return builder.toString()
+    }
+
+    static BufferedImage loadImage(String path) throws Exception {
+        try {
+            return ImageIO.read(new File(RESOURCES_DIRECTORY + path))
+        } catch (IOException e) {
+            e.printStackTrace()
+            throw new Exception("Image " + RESOURCES_DIRECTORY + path + " not found")
+        }
     }
 
 }
